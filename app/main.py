8b8cf8ac9680c97bb587e5cfc05aa56439545d22
Andrii Mruts <anmrut@apollolims.com>
2021-09-13 01:03:51 +0300

    modify welcome message

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World!!!"}
+    return {"message": "Тобі вдалося, красень! Ти найшикарніша ічтота на світі!!!"}
 
 
 app.include_router(api_router, prefix="/api/v1")
