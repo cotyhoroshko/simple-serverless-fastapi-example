85d24837fa505bca6ab81d4ff7dc3e8ed6005654
Andrii Mruts <anmrut@apollolims.com>
2021-09-13 01:36:01 +0300

    add environment variables handling

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 from app.api.api_v1.api import router as api_router
+from app.core import config
 from mangum import Mangum
 
 app = FastAPI()
@@ -8,8 +9,9 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Тобі вдалося, красень! Ти найшикарніша ічтота на світі!!!"}
+    return {"message": f"Тобі вдалося, красень! Ти найшикарніша істота на світі!!!"
+                       f" \n Secret Key: {config.settings.secret_key}"}
 
 
-app.include_router(api_router, prefix="/api/v1")
+app.include_router(api_router, prefix=config.settings.prefix)
 handler = Mangum(app)
